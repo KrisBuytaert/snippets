e48697b169671374a61858b0faefce67f4d82295
Kris Buytaert <sdog@inuits.be>
2012-01-22 10:01:45 +0100

    Added, todolist,  removed unused arrays.

--- monitoring/tablestats.php
+++ monitoring/tablestats.php
@@ -1,14 +1,22 @@
 <?php
 
-# Prefix String for Graphite 
+# TODO externalize config,   load mysql config from a config file 
+# TODO make schema and tables to graph / monitor configurable , currently grabbing everythin :(
+
+# Prefix String for Graphite  / maybe replace by uname -a + schema name 
 $basename   = "mss.myss";
 # MySQL Connection 
 $dbname     = "myss";
+
+
+# 
 $mysql_host = '127.0.0.1';
 $mysql_user = 'myss';
 $mysql_pass = '1234';
 $mysql_port = 3306;
-$mysql_ssl  = FALSE;   # Whether to use SSL to connect to MySQL.
+
+
+
 
 # You should not need to change anything below this line.  
 
@@ -25,22 +33,19 @@ $mysql_ssl  = FALSE;   # Whether to use SSL to connect to MySQL.
     if ( !$conn ) {
         die("MySQL: " . mysql_error());
     }
-    
+
+    # Set Time 
     $timestamp = time();
     $sql = "SHOW TABLES FROM $dbname";
     $result = @mysql_query($sql, $conn);
-    $resultset = array();
     while ( $row = @mysql_fetch_array($result) ) {
-        $resultset[] = $row;
         # print "$row[0] \n ";
         $detailsql = "
             select TABLE_ROWS, AVG_ROW_LENGTH,  DATA_LENGTH, MAX_DATA_LENGTH, INDEX_LENGTH  
             from information_schema.tables
             where table_name='$row[0]'";
         $detail = @mysql_query ($detailsql,$conn);
-        $detailset = array();
         while ( $drow = @mysql_fetch_array($detail)) {
-            $detailset = $drow;
             print "$basename.$row[0]-nrrows      $drow[0]  $timestamp \n"; 
             print "$basename.$row[0]-avglength   $drow[1]  $timestamp \n";
             print "$basename.$row[0]-datalength  $drow[2]  $timestamp \n";
